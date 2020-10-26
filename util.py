502dc558709f3830c727d12fb3b1715fb1ad8b8a
iamernie8199 <iamernie8199@gmail.com>
2020-10-26 14:13:05 +0800

    Update util.py

--- util.py
+++ util.py
@@ -3,7 +3,9 @@ import scipy.stats as stat
 import matplotlib.pyplot as plt
 import statsmodels.tsa.api as smt
 import statsmodels.api as sm
+import statsmodels.stats as sm_stat
 import scipy.stats as scs
+import numpy as np
 
 
 def basicStats(x):
@@ -33,12 +35,13 @@ def tsplot(y, lags=None, figsize=(10, 8), style='seaborn'):
         y = pd.Series(y)
     with plt.style.context(style):
         fig = plt.figure(figsize=figsize)
-        layout = (3, 2)
+        layout = (4, 2)
         ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
         acf_ax = plt.subplot2grid(layout, (1, 0))
         pacf_ax = plt.subplot2grid(layout, (1, 1))
         qq_ax = plt.subplot2grid(layout, (2, 0))
         pp_ax = plt.subplot2grid(layout, (2, 1))
+        hist = plt.subplot2grid(layout, (3, 0), colspan=2)
 
         y.plot(ax=ts_ax)
         ts_ax.set_title('Time Series Analysis Plots')
@@ -47,5 +50,23 @@ def tsplot(y, lags=None, figsize=(10, 8), style='seaborn'):
         sm.qqplot(y, line='s', ax=qq_ax)
         qq_ax.set_title('QQ Plot')
         scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+        y.hist(bins=40,ax=hist)
+        hist.set_title('Histogram')
         plt.tight_layout()
         plt.show()
+
+def tsdiag(y, figsize = (14, 8), title = "", lags = 30):
+    tmp_data = pd.Series(y)
+    tmp_data.index += 1
+    tmp_acor = list(sm_stat.diagnostic.acorr_ljungbox(tmp_data, lags = lags, boxpierce = True))
+    # Plot Ljung-Box and Box-Pierce statistic p-values:
+    plt.plot(range(1, len(tmp_acor[0]) + 1), tmp_acor[1], 'bo', label = "Ljung-Box values")
+    plt.plot(range(1, len(tmp_acor[0]) + 1), tmp_acor[3], 'go', label = "Box-Pierce values")
+    plt.xticks(np.arange(1,  len(tmp_acor[0]) + 1, 1.0))
+    plt.axhline(y = 0.05, color = "red", label = "5% critical value")
+    plt.title("$Time\ Series\ " + title + "$")
+    plt.legend()
+    plt.show()
+    # Return the statistics:
+    col_index = ["Ljung-Box: X-squared", "Ljung-Box: p-value", "Box-Pierce: X-squared", "Box-Pierce: p-value"]
+    return pd.DataFrame(tmp_acor, index = col_index, columns = range(1, len(tmp_acor[0]) + 1))
