05d0af2c2481e3f671405d39178b04689c1ae127
iamernie8199 <iamernie8199@gmail.com>
2020-10-27 19:02:47 +0800

    Update Example ARCH Effect test.py

--- ch3/Example ARCH Effect test.py
+++ ch3/Example ARCH Effect test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.diagnostic import acorr_ljungbox
+from statsmodels.stats.diagnostic import acorr_ljungbox, het_arch
 
 
 def ljungbox(data, lags=12):
@@ -11,9 +11,19 @@ def ljungbox(data, lags=12):
     print(f"p-value: {blres.tail(1)['lb_pvalue'].values[0]}")
 
 
+def engle(data, lags=12):
+    arch = het_arch(data, nlags=lags)
+    print("Lagrange multiplier test")
+    print(f"Chi-squared: {round(arch[0], 4)}", end=", ")
+    print(f"p-value: {arch[1]}")
+
+
 da = pd.read_csv('../data/m-intc7308.txt', delimiter='\s+')
 intc = np.log(da['rtn'] + 1)
 ljungbox(intc)
 
 at = intc - intc.mean()
 ljungbox(pow(at, 2))
+
+# Engle’s Test
+engle(intc)
