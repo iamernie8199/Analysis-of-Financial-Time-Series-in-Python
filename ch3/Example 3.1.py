b94807582caca7a4f7bd36ad8a256174223c47c3
iamernie8199 <iamernie8199@gmail.com>
2020-10-29 23:02:41 +0800

    Update Example 3.1.py

--- ch3/Example 3.1.py
+++ ch3/Example 3.1.py
@@ -3,6 +3,7 @@ import numpy as np
 from arch import arch_model
 from statsmodels.stats.diagnostic import acorr_ljungbox
 import matplotlib.pyplot as plt
+from math import sqrt
 
 da = pd.read_csv('../data/m-intc7308.txt', delimiter='\s+')
 intc = np.log(da['rtn'] + 1)
@@ -21,5 +22,8 @@ print(f"p-value: {blres.tail(1)['lb_pvalue'].values[0]}")
 
 print(arch1.arch_lm_test(lags=10, standardized=True))
 
+# unconditional standard error
+print(f"unconditional standard error: {sqrt(arch1.params[1] / (1 - arch1.params[2]))}")
+# fitted volatility series
 arch1.plot()
 plt.show()
