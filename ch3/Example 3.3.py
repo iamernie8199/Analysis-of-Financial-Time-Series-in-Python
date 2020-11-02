6547cc320d59933de0d49c388569c3775b7b20cc
iamernie8199 <iamernie8199@gmail.com>
2020-11-02 14:59:27 +0800

    Update Example 3.3.py

--- ch3/Example 3.3.py
+++ ch3/Example 3.3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
+import arch
 
 da = pd.read_fwf('../data/sp500.txt')
 da.plot()
@@ -19,3 +20,16 @@ print(f"sigma: {res.params['sigma2'] ** 0.5}")
 # fit ar(3)
 ar = ARIMA(da, order=(3, 0, 0)).fit()
 print(ar.summary())
+
+# fit a GARCH(1,1) model on the residuals of the ar(3) model
+garch = arch.arch_model(ar.resid, p=1, q=1).fit(update_freq=0)
+print(garch.summary())
+# the implied unconditional variance of a_t
+print(
+    f"unconditional variance of a_t: {garch.params['omega'] / (1 - garch.params['alpha[1]'] - garch.params['beta[1]'])}")
+
+# dropping all AR parameters
+ar0 = ARIMA(da, order=(0, 0, 0)).fit()
+print(ar0.summary())
+garch = arch.arch_model(ar0.resid, p=1, q=1).fit(update_freq=0)
+print(garch.summary())
